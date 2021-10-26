f4ae67a7a3c080ad249cbba9af4d99ad446f8854
LeoSirius <is.li.xiaoyu@qq.com>
2021-10-26 11:09:41 +0800

    update

--- cp1_a_tutorial_introduction/03_f2c_float.c
+++ cp1_a_tutorial_introduction/03_f2c_float.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300 */
+    for fahr = 0, 20, ..., 300; floating-point version */
 main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;         /* lower limit of temperature scale */
     upper = 300;       /* upper limit */
@@ -13,8 +13,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3d %6d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
