3c454b2700175ee87db14210d0669f2605d2335f
LeoSirius <is.li.xiaoyu@qq.com>
2021-10-30 22:07:11 +0800

    update

--- 02_types_operators_and_expressions/exercise/2-8.c
+++ 02_types_operators_and_expressions/exercise/2-8.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+int wordLength(void)
+{
+    int i;
+    unsigned v = (unsigned)~0;
+    for (i = 1; ((v = v >> 1) & 1) > 0; ++i)
+        ;
+    return i;
+}
+
+unsigned rightrot(unsigned x, int n)
+{
+    int rbit;
+    int word_length = wordLength();
+    while(n--) {
+        rbit = x & 1;
+        x >>= 1;
+        x |= rbit << (word_length-1);
+    }
+    return x;
+}
+
 int main()
 {
-
+    printf("%X\n", rightrot(0xF2, 4));
     return 0;
 }
 
